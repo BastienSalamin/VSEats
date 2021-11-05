50e99f1eb5a6f9fd19d2812e4f8c5b7cacbd4a0a
alex <amj@mac3.ch>
2021-11-05 15:57:19 +0100

    BLL 1st iteration

---
+++ BLL/UtilisateursManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DAL;
+using DTO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace BLL
+{
+    public class UtilisateursManager
+    {
+        // Création de deux références privées
+        private IUtilisateursDB utilisateursDb { get; }
+
+
+        // Création du constructeur pour instancier la DAL
+        public UtilisateursManager(IConfiguration configuration)
+        {
+            utilisateursDb = new UtilisateursDB(configuration);
+            
+        }
+
+
+        //liste des méthodes utilisateurs
+
+        public List<Utilisateurs> GetUtilisateurs()
+        {
+            return utilisateursDb.GetUtilisateurs();
+        }
+    }
+}
