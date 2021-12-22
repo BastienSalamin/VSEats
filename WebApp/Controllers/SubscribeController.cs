6a97b8e11e95bcddb73a520c7e66442ba1fc68a6
Bastien Salamin <bastien.salamin@hes-so.ch>
2021-12-22 22:27:07 +0100

    Edit avec SubscribeVM

--- WebApp/Controllers/SubscribeController.cs
+++ WebApp/Controllers/SubscribeController.cs
@@ -11,23 +11,73 @@ namespace WebApp.Controllers
     public class SubscribeController : Controller
     {
         private IUtilisateursManager UtilisateursManager { get; }
+        public ILocalitesManager LocalitesManager { get; set; }
 
-        public SubscribeController(IUtilisateursManager utilisateursManager)
+        public SubscribeController(IUtilisateursManager utilisateursManager, ILocalitesManager localitesManager)
         {
             UtilisateursManager = utilisateursManager;
+            LocalitesManager = localitesManager;
         }
         public IActionResult Index()
         {
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(SubscribeVM subscribeVM)
+        {
+            if (ModelState.IsValid)
+            {
+                // vérifier qu'il n'y ait pas d'utilisateurs avec le même login
+                var listeUsers = UtilisateursManager.GetUtilisateurs();
+
+                foreach (var user in listeUsers)
+                {
+                    if (user.Login.Contains(subscribeVM.Login))
+                    {
+                        ModelState.AddModelError("", "Cet e-mail est déjà utilisé !");
+                        return View(subscribeVM);
+                    }
+                }
+
+                UtilisateursManager.Subscribe(subscribeVM.Npa, subscribeVM.Nom, subscribeVM.Prenom, subscribeVM.Login, subscribeVM.MotDePasse, subscribeVM.Adresse, subscribeVM.NumTelephone);
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Erreur lors de l'insertion !");
+            }
+
+            return View(subscribeVM);
         }
 
         //pour Edit les données
         public IActionResult Edit(int id)
         {
             var util = UtilisateursManager.GetUserId(id);
+            var localites = LocalitesManager.GetLocalites();
+            int npa = 0;
 
-            return View(util);
+            foreach (var localite in localites)
+            {
+                if(localite.IdLocalite == util.IdLocalite)
+                {
+                    npa = localite.NPA;
+                }
+            }
+
+            SubscribeVM membre = new SubscribeVM();
+            membre.Npa = npa;
+            membre.Nom = util.Nom;
+            membre.Prenom = util.Prenom;
+            membre.Login = util.Login;
+            membre.MotDePasse = util.MotDePasse;
+            membre.Adresse = util.Adresse;
+            membre.NumTelephone = util.NumTelephone;
+
+            return View(membre);
         }
 
         [HttpPost]
@@ -43,34 +93,5 @@ namespace WebApp.Controllers
             return View(subscribeVM);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Index(SubscribeVM subscribeVM)
-        {
-            if (ModelState.IsValid)
-            {
-                // vérifier qu'il n'y ait pas d'utilisateurs avec le même login
-                var listeUsers = UtilisateursManager.GetUtilisateurs();
-
-                foreach (var user in listeUsers)
-                {
-                    if(user.Login.Contains(subscribeVM.Login))
-                    {
-                        ModelState.AddModelError("", "Cet e-mail est déjà utilisé !");
-                        return View(subscribeVM);
-                    }
-                }
-                
-                UtilisateursManager.Subscribe(subscribeVM.Npa, subscribeVM.Nom, subscribeVM.Prenom, subscribeVM.Login, subscribeVM.MotDePasse, subscribeVM.Adresse, subscribeVM.NumTelephone);
-                return RedirectToAction("Index", "Home");
-
-            }
-            else
-            {
-                ModelState.AddModelError("", "Erreur lors de l'insertion !");
-            }
-
-            return View(subscribeVM);
-        }
     }
 }
